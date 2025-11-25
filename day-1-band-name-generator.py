419071deb05d21a3ce0dd273021a3e18682e1ac6
Victor Mancuzo <100636697+victormancuzo@users.noreply.github.com>
2025-11-25 13:10:10 -0300

    feat: add Band Name Generator
    Dia 1: aplicação simples em Python que gera um nome de banda baseado nas respostas do usuário.
    
    Aprendizados:
    - Entrada e saída de dados no terminal usando `input()` e `print()`
    - Manipulação básica de strings

---
+++ day-1-band-name-generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator!")
+city_name = input("What's the name of the city you grew up in?\n")
+pets_name = input("What's your pet's name?\n")
+print(f"Your band name could be {city_name} {pets_name}!")
